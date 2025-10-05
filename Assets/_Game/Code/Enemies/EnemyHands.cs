331220485bc59ed15b926f71684529723cbafb28
NesCafe62 <1944556+NesCafe62@users.noreply.github.com>
2025-10-05 22:04:41 +0300

    fix hands appear & ghost sprites

--- Assets/_Game/Code/Enemies/EnemyHands.cs
+++ Assets/_Game/Code/Enemies/EnemyHands.cs
@@ -4,26 +4,20 @@ class EnemyHands : MonoBehaviour
 {
 
 	[SerializeField] float grabRange = 1;
-	// [SerializeField] float targetLoseRange = 3;
-
-	/* void Update()
-	{
-		Vector2 playerPos = G.run.Player.transform.position;
-		Vector2 delta = playerPos - (Vector2) transform.position;
-		if (delta.sqrMagnitude > targetLoseRange)
-		{
-			Destroy(gameObject);
-		}
-	} */
 
 	void GrabPlayer()
 	{
 		Vector2 playerPos = G.run.Player.transform.position;
-		Vector2 delta = playerPos - (Vector2) transform.position;
+		Vector2 delta = playerPos - (Vector2)transform.position;
 		if (delta.magnitude <= grabRange)
 		{
 			G.run.Player.GetComponent<PlayerController>().CatchByHands();
 		}
+	}
+
+	void AnimationEnded()
+	{
+		Destroy(transform.parent.gameObject);
 	}
 
 }
